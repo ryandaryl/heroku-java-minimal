9cd35361b1b34129fe183a2bebc517f9dbc15b91
ryandaryl <ryandarylmills@gmail.com>
2018-01-21 02:35:13 +0000

    Render template to browser.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -jar target/heroku-java-minimal-1.0.jar
+web: java -Dserver.port=$PORT -jar target/heroku-java-minimal-1.0.jar
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,8 +1,24 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+@SpringBootApplication
 public class Main {
 
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
-    }
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(Main.class, args);
+  }
+  
+  @RequestMapping("/")
+  String hello(Map<String, Object> model) {
+    model.put("mytext", "Hello from Java!");
+    return "hello";
+  }  
 
 }
